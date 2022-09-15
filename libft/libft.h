7bc50a50bc35daa2300fe8fdf1d8c05211f54e6f
Daniel <drmoreno271@gmail.com>
2022-09-15 13:57:10 +0200

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -13,17 +13,16 @@
 #ifndef LIBC_H
 #define LIBC_H
 
-int             ft_isalpha(int c);
-int             ft_isdigit(int c);
 int             ft_isalnum(int c);
+int             ft_isalpha(int c);
 int             ft_isascii(int c);
+int             ft_isdigit(int c);
 int             ft_isprint(int c);
-unsigned int	ft_strlen(char *s);
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
+unsigned int	ft_strlen(char *s);
 int             ft_strncmp(char *s1, char *s2, unsigned int n);
 int             ft_toupper(int c);
 int             ft_tolower(int c);
-int             ft_strncmp(char *s1, char *s2, unsigned int n);
 
 #endif
