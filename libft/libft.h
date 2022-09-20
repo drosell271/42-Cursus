42e1e607863b3307df6e601d3fd7f10ea11772c9
Daniel <drmoreno271@gmail.com>
2022-09-20 10:58:43 +0200

    otro mas

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 19:17:31 by drosell-          #+#    #+#             */
-/*   Updated: 2022/09/20 10:18:09 by drosell-         ###   ########.fr       */
+/*   Updated: 2022/09/20 10:53:14 by drosell-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 # include <stdlib.h>
 
 int		ft_atoi(const char *str);
-//char	*strchr(const char *s, int c);
 void	ft_bzero(void *s, size_t n);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+void 	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *s, int c, size_t n);
