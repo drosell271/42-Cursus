3ab8d8aaf417348b4177bdd2fcfa0adf267a96a5
Daniel <drmoreno271@gmail.com>
2022-09-20 17:08:04 +0200

    more

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -1,31 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:09:00 by drosell-          #+#    #+#             */
-/*   Updated: 2022/09/20 17:06:20 by drosell-         ###   ########.fr       */
+/*   Updated: 2022/09/20 17:06:49 by drosell-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
 	int	count;
 
 	count = 0;
-	if (!s)
-		return (NULL);
 	while (s[count])
+		count++;
+	while (count >= 0)
 	{
 		if (s[count] == (char) c)
 			return ((char *)(s + count));
-		count++;
+		count--;
 	}
-	if (s[count] == (char) c)
-		return ((char *)(s + count));
 	return (NULL);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 19:17:31 by drosell-          #+#    #+#             */
-/*   Updated: 2022/09/20 11:03:30 by drosell-         ###   ########.fr       */
+/*   Updated: 2022/09/20 16:52:22 by drosell-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,12 @@ int 	ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *s, int c, size_t n);
+char 	*ft_strchr(const char *s, int c);
 size_t	ft_strlcat(char *dest, char *src, size_t size);
 size_t	ft_strlcpy(char *dest, char *src, size_t size);
 size_t	ft_strlen(char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strrchr(const char *s, int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 
