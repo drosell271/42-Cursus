95c30468d66e9f13bb0e3ffa39e1df8dc73e5f4f
Daniel <drmoreno271@gmail.com>
2022-09-23 14:34:46 +0200

    to bien

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 15:25:44 by drosell-          #+#    #+#             */
-/*   Updated: 2022/09/23 14:12:53 by drosell-         ###   ########.fr       */
+/*   Updated: 2022/09/23 14:25:29 by drosell-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 
 int	ft_atoi(const char *str)
 {
-	unsigned int	num;
-	int				i;
-	long int		np;
+	long long	num;
+	int			i;
+	int			np;
 
 	np = 1;
 	i = 0;
 	num = 0;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\f'
-		|| str[i] == '\r' || str[i] == '\n' || str[i] == '\v')
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 14))
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 		if (str[i++] == '-')
@@ -30,7 +29,11 @@ int	ft_atoi(const char *str)
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		num = num * 10 + (str[i] - '0');
+		if (num * np > INT_MAX)
+			return (-1);
+		if (num * np < INT_MIN)
+			return (0);
 		i++;
 	}
-	return ((int)(np * num));
+	return (np * num);
 }
