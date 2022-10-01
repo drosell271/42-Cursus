41767f8b61d7d8d969fe45d799e470112cdcfe40
Daniel <drmoreno271@gmail.com>
2022-10-01 17:14:36 +0200

    Solo falta el map

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 19:17:31 by drosell-          #+#    #+#             */
-/*   Updated: 2022/10/01 15:51:51 by drosell-         ###   ########.fr       */
+/*   Updated: 2022/10/01 17:14:15 by drosell-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,9 +54,9 @@ char	*ft_strtrim(char const *s1, char const *set);
 //Bonus
 typedef struct s_list
 {
-	void 			*content;
-	struct 	s_list 	*next;
-} 					t_list;
+	void			*content;
+	struct s_list	*next;
+}					t_list;
 
 t_list	*ft_lstnew(void *nuevo);
 void	ft_lstadd_front(t_list **lst, t_list *new);
