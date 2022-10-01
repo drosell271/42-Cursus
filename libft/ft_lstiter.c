abe24f54430379581165950a0c6fe6a7d0f85369
Daniel <drmoreno271@gmail.com>
2022-10-01 15:41:25 +0200

    casi listo

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 17:35:04 by drosell-          #+#    #+#             */
-/*   Updated: 2022/09/30 18:35:15 by drosell-         ###   ########.fr       */
+/*   Updated: 2022/10/01 15:38:04 by drosell-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	
+	t_list	*temp;
+
+	if (lst && f)
+	{
+		while (lst)
+		{
+			f (lst->content);
+			temp = lst->next;
+			lst = temp;
+		}
+	}
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 19:17:31 by drosell-          #+#    #+#             */
-/*   Updated: 2022/09/30 18:37:55 by drosell-         ###   ########.fr       */
+/*   Updated: 2022/10/01 15:19:38 by drosell-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,6 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
-//void	ft_lstiter(t_list *lst, void (*f)(void *));
-//t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
