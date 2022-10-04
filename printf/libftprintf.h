61e71eba3a7303891bd472579b9038c5a9c91dbc
Daniel <drmoreno271@gmail.com>
2022-10-04 16:51:21 +0200

    start

---
+++ printf/libftprintf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 16:43:46 by drosell-          #+#    #+#             */
+/*   Updated: 2022/10/04 16:47:59 by drosell-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+# include <stdlib.h>
+# include <limits.h>
+
+//Imprimir valores
+
+//Leer valores
+
+//Funciones
+
+#endif
