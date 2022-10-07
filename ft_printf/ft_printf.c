68315726302e0cab3dd3c024f4c4c1f5f3ea873c
Daniel <drmoreno271@gmail.com>
2022-10-07 19:01:39 +0200

    avanzamos bastante

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,45 +6,76 @@
 /*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 14:12:11 by drosell-          #+#    #+#             */
-/*   Updated: 2022/10/07 15:00:12 by drosell-         ###   ########.fr       */
+/*   Updated: 2022/10/07 19:01:09 by drosell-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "libftprintf.h"
 
-int ft_printf(const char *input, ...)
+int	select_mode(char input_2, va_list ptr)
+{
+	if (input_2 == 'c')
+		return (mode_c(ptr));
+	else if (input_2 == 's')
+		return (mode_s(ptr));
+	/*else if (input_2 == 'p')
+		return (mode_p((char *) va_arg (ptr, int)));*/
+	else if (input_2 == 'd' || input_2 == 'i')
+		return (mode_d_i(va_arg (ptr, int), 0));
+	else if (input_2 == 'u')
+		return (mode_u(va_arg (ptr, unsigned int), 0));
+	/*else if (input_2 == 'x')
+		mode_x();
+	else if (input_2 == 'X')
+		mode_X_mayus();
+	else if (input_2 == '%')
+		mode_percent();
+	else*/
+		return (0);
+}
+
+int	ft_printf(const char *input, ...)
 {
 	va_list	ptr;
 	int		counter_input;
 	int		total_print;
 
 	counter_input = 0;
+	total_print = 0;
 	va_start (ptr, input);
-	while (input[counter_input])
+	while ((char) input[counter_input])
 	{
-		if (input[counter_input] == '%')
+		if ((char) input[counter_input] == '%')
 		{
-			if (input[counter_input + 1] != '%')
-			{
-					
-			}
-			else
-			{
-				write (1, '%', 1);
-				counter_input++;
-			}
+			total_print += select_mode((char) input[counter_input + 1], ptr);
+			counter_input++;
 		}
 		else
 		{
 			write(1, &input[counter_input], 1);
+			total_print++;
 		}
-		if (input[counter_input])
+		if ((char) input[counter_input] == '%'
+			&& (char) input[counter_input + 1] == '%')
+			counter_input += 2;
+		else
 			counter_input++;
 	}
-	
-	
-
-
+	va_end (ptr);
 	return (total_print);
+}
+
+int	main()
+{
+	int var1;
+	int var2;
+	
+	printf("P11: hola como estás\n");
+	ft_printf("P12: hola como estás\n");
+
+	var1 = printf("P21: hola %u adios\n", 1234);
+	var2 = ft_printf("P22: hola %u adios\n", 1234);
+	printf("%d\n", var1);
+	printf("%d\n", var2);
+	return (0);
 }
--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -6,20 +6,27 @@
 /*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 16:43:46 by drosell-          #+#    #+#             */
-/*   Updated: 2022/10/07 14:36:52 by drosell-         ###   ########.fr       */
+/*   Updated: 2022/10/07 19:00:53 by drosell-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 # include <stdlib.h>
+# include <unistd.h>
 # include <stdarg.h>
 # include <limits.h>
 
-//Imprimir valores
+# include <stdio.h>
 
-//Leer valores
+//ft_printf.c
+int	ft_printf(const char *input, ...);
+int	select_mode(char input_2, va_list ptr);
 
-//Funciones
+//ft_printf_bonus.c
+int	mode_c(va_list ptr);
+int	mode_s(va_list ptr);
+int mode_d_i(int n, int output);
+int mode_u(unsigned int n , int output);
 
 #endif
