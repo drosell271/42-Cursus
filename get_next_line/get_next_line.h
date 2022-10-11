def62b5558c2e077dd71963a0bfd18d424f776d4
Daniel <drmoreno271@gmail.com>
2022-10-11 10:27:24 +0200

    Inicio de get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 10:21:56 by drosell-          #+#    #+#             */
+/*   Updated: 2022/10/11 10:21:56 by drosell-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+//get_next_line.c
+
+
+//get_next_line_utils.c
+
+
+
+#endif
