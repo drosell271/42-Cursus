f8d4bb100df7d1a3eaa5ac330d01d194fa625b89
Daniel <drmoreno271@gmail.com>
2023-02-24 17:47:31 +0100

    Minitalk con truco

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 22:26:30 by drosell-          #+#    #+#             */
-/*   Updated: 2023/02/22 22:26:30 by drosell-         ###   ########.fr       */
+/*   Updated: 2023/02/24 17:47:05 by drosell-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,43 +15,39 @@
 #include <unistd.h>
 #include "libft/libft.h"
 
-// Función para manejar las señales SIGUSR1 y SIGUSR2
-static void signal_handler(int signal)
+static void	signal_handler(int signal)
 {
-	static int received_signals = 0;
+	static int	received_signals = 0;
 
 	if (signal == SIGUSR1)
-		++received_signals; // Aumenta el contador de señales recibidas
+		++received_signals;
 	else
 	{
-		ft_putnbr_fd(received_signals, 1); // Escribe en la salida estándar la cantidad de señales recibidas
-		ft_putchar_fd('\n', 1);			   // Escribe un salto de línea en la salida estándar
-		exit(0);						   // Termina el proceso
+		ft_putnbr_fd(received_signals, 1);
+		ft_putchar_fd('\n', 1);
+		exit(0);
 	}
 }
 
-// Función para enviar señales a otro proceso
-static void send_signals(int pid, char *string)
+static void	send_signals(int pid, char *string)
 {
-	int i;
-	char c;
-	// Itera por cada caracter en la cadena a enviar
+	int		i;
+	char	c;
+
 	while (*string)
 	{
-		i = 8;		   // Número de bits en un byte
-		c = *string++; // Obtiene el caracter y avanza al siguiente
-		// Itera por cada bit en el caracter
+		i = 8;
+		c = *string++;
 		while (i--)
 		{
-			if (c >> i & 1)			// Si el bit es 1
-				kill(pid, SIGUSR2); // Envía la señal SIGUSR2
+			if (c >> i & 1)
+				kill(pid, SIGUSR2);
 			else
-				kill(pid, SIGUSR1); // Si el bit es 0, envía la señal SIGUSR1
-			usleep(100);			// Espera un tiempo para enviar la siguiente señal
+				kill(pid, SIGUSR1);
+			usleep(250);
 		}
 	}
 	i = 8;
-	// Envía 8 señales SIGUSR1 para indicar que se ha terminado de enviar la cadena
 	while (i--)
 	{
 		kill(pid, SIGUSR1);
@@ -59,19 +55,18 @@ static void send_signals(int pid, char *string)
 	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	// Verifica que se han pasado los argumentos requeridos
 	if (argc != 3 || !ft_strlen(argv[2]))
 		return (1);
 	ft_putstr_fd("Sent : ", 1);
-	ft_putnbr_fd(ft_strlen(argv[2]), 1); // Escribe en la salida estándar la cantidad de caracteres en la cadena a enviar
+	ft_putnbr_fd(ft_strlen(argv[2]), 1);
 	ft_putchar_fd('\n', 1);
 	ft_putstr_fd("Received: ", 1);
-	signal(SIGUSR1, signal_handler);		 // Asigna la función signal_handler a la señal SIGUSR1
-	signal(SIGUSR2, signal_handler);		 // Asigna la función signal_handler a la señal SIGUSR2
-	send_signals(ft_atoi(argv[1]), argv[2]); // Envía las señales
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+	send_signals(ft_atoi(argv[1]), argv[2]);
 	while (1)
-		pause(); // Pausa el proceso hasta que se reciba una señal
+		pause();
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: drosell- <drosell-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 22:26:33 by drosell-          #+#    #+#             */
-/*   Updated: 2023/02/22 22:26:33 by drosell-         ###   ########.fr       */
+/*   Updated: 2023/02/24 17:47:09 by drosell-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,63 +15,45 @@
 #include "libft/libft.h"
 
 // Definición de la función que maneja la señal recibida
-static void handler(int sig, siginfo_t *info, void *context)
+static void	handler(int sig, siginfo_t *info, void *context)
 {
-  static int bit_index = 0;           // Índice del bit actual
-  static pid_t client_pid = 0;        // PID del cliente
-  static unsigned char message = 0;   // Mensaje recibido
+	static int				bit_index = 0;
+	static pid_t			client_pid = 0;
+	static unsigned char	message = 0;
 
-  (void)context;  // Evita advertencias sobre la variable no utilizada
-
-  if (!client_pid)
-    client_pid = info->si_pid;
-
-  // Lee el bit recibido
-  message |= (sig == SIGUSR2);
-
-  // Verifica si ya se leyeron los 8 bits del mensaje
-  if (++bit_index == 8)
-  {
-    bit_index = 0;
-
-    // Si el mensaje es el carácter nulo, indica que se ha terminado la transmisión
-    if (!message)
-    {
-      // Envia la señal SIGUSR2 al cliente para indicar que se ha terminado la transmisión
-      kill(client_pid, SIGUSR2);
-      client_pid = 0;
-      return ;
-    }
-
-    // Muestra el carácter recibido por la salida estándar
-    ft_putchar_fd(message, 1);
-    message = 0;
-
-    // Envia la señal SIGUSR1 al cliente para indicar que está listo para recibir el siguiente carácter
-    kill(client_pid, SIGUSR1);
-  }
-  else
-    message <<= 1;
+	(void)context;
+	if (!client_pid)
+		client_pid = info->si_pid;
+	message |= (sig == SIGUSR2);
+	if (++bit_index == 8)
+	{
+		bit_index = 0;
+		if (!message)
+		{
+			kill(client_pid, SIGUSR2);
+			client_pid = 0;
+			return ;
+		}
+		ft_putchar_fd(message, 1);
+		message = 0;
+		kill(client_pid, SIGUSR1);
+	}
+	else
+		message <<= 1;
 }
 
 int	main(void)
 {
-  struct sigaction signal_action;
+	struct sigaction	signal_action;
 
-  // Muestra el PID del servidor
-  ft_putstr_fd("Server PID: ", 1);
-  ft_putnbr_fd(getpid(), 1);
-  ft_putchar_fd('\n', 1);
-
-  // Registra la función manejadora de señales
-  signal_action.sa_sigaction = handler;
-  signal_action.sa_flags = SA_SIGINFO;
-  sigaction(SIGUSR1, &signal_action, 0);
-  sigaction(SIGUSR2, &signal_action, 0);
-
-  // Espera a que lleguen señales
-  while (1)
-    pause();
-
-  return (0);
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
+	signal_action.sa_sigaction = handler;
+	signal_action.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &signal_action, 0);
+	sigaction(SIGUSR2, &signal_action, 0);
+	while (1)
+		pause();
+	return (0);
 }
